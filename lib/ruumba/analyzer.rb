13b447843cc79b00ea87caa46beb960f342a45eb
Jan Biniok <jan.biniok@raritan.com>
2017-05-17 14:54:34 +0200

    Rework to use a temporary folder instead of creating files in parallel Add command line option to define a temporary folder for generated files Passthrough some Rubocop switches (--auto-get-config, --config, ...)

--- lib/ruumba/analyzer.rb
+++ lib/ruumba/analyzer.rb
@@ -1,6 +1,7 @@
 # @author Eric Weinstein <eric.q.weinstein@gmail.com>
 
 require 'securerandom'
+require 'pathname'
 
 # Ruumba: RuboCop's sidekick.
 module Ruumba
@@ -8,24 +9,44 @@ module Ruumba
   # to RuboCop for linting (style, correctness, &c).
   class Analyzer
     # The regular expression to capture interpolated Ruby.
-    ERB_REGEX = /<%=?(.*?)%>/
+    ERB_REGEX = /<%=?(.*?)%>/m
+
+    def initialize(opts = nil)
+      @options = opts || {}
+    end
 
     # Performs static analysis on the provided directory.
     # @param [Array<String>] dir The directory to analyze.
     def run(dir = ARGV)
-      fq_dir = File.expand_path dir.first
-      suffix = SecureRandom.hex
+      fq_dir = Pathname.new(File.expand_path(dir.first))
+      if @options[:tmp_folder]
+        tmp = Pathname.new(File.expand_path(@options[:tmp_folder]))
+        FileUtils.rm_rf(tmp)
+      else
+        tmp = Pathname.new("#{fq_dir}/ruumba_tmp_#{SecureRandom.hex[0..3]}/")
+      end
 
       Dir["#{fq_dir}/**/*.erb"].each do |f|
-        File.open("#{f}-#{suffix}.rb", 'w+') do |file|
+        n = tmp + Pathname.new(f).relative_path_from(fq_dir)
+        FileUtils.mkdir_p(File.dirname(n))
+
+        File.open("#{n}.rb", 'w+') do |file|
           code = extract f
           file.write code
         end
       end
 
-      system("rubocop #{fq_dir}")
+      if @options && @options[:arguments]
+        args = @options[:arguments].join(' ')
+      else
+        args = ''
+      end
 
-      Dir.glob(Dir["#{fq_dir}/**/*-#{suffix}.rb"]).each { |f| File.delete(f) }
+      if !@options[:tmp_folder]
+        FileUtils.rm_rf(tmp)
+      end
+
+      system("rubocop #{args} #{tmp}")
     end
 
     # Extracts Ruby code from an ERB template.
